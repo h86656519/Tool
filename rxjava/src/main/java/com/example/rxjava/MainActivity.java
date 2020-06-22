15edf72e96ce19bf8f24b68b7f48933edf958d4d
leosu1111 <leosu@palmbox.com.tw>
2020-06-22 21:52:57 +0800

    功能實做 interval

--- rxjava/src/main/java/com/example/rxjava/MainActivity.java
+++ rxjava/src/main/java/com/example/rxjava/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 import com.jakewharton.rxbinding3.view.RxView;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
@@ -24,10 +25,12 @@ import io.reactivex.schedulers.Schedulers;
  * 1.利用Rxjava 來擋住過快的二次連點，防手抖
  * 2.建立一個非同步事件(ex:呼api，撈db 等等...)
  * 3.建立多個任務同時執行，優化寫法
+ * 4.功能實做 interval，在task2上
  */
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     Button btnOk;
+    CountDownLatch latch = new CountDownLatch(1);
 
     @SuppressLint("CheckResult")
     @Override
@@ -44,32 +47,36 @@ public class MainActivity extends AppCompatActivity {
         Observable<Integer> observableTask1 = task1();
         Observable<Integer> observableTask2 = task2();
 
-        observableTask1.subscribe(observer);
+        //  observableTask1.subscribe(observer);
         observableTask2.subscribe(observer);
     }
 
     private Observable<Integer> task2() {
-        return Observable.create(new ObservableOnSubscribe<Integer>() {
-                @Override
-                public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
-                    for (int i = 1; i <= 15; i++) {
-                        try {
-                            Thread.sleep(1000);
-                            if (i == 15) {
-                                emitter.onNext(15);
+        return Observable.
+                interval(1, TimeUnit.SECONDS) //每個1秒呼一次 onNext
+                .create(new ObservableOnSubscribe<Integer>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                        for (int i = 1; i <= 15; i++) {
+                            try {
+                                Thread.sleep(1000);
+                                //  if (i == 15) {
+//                                    emitter.onNext(15);
+                                emitter.onNext(i);
+                                //}
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
                             }
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
                         }
                     }
-                }
-            }).subscribeOn(Schedulers.io());
+                }).subscribeOn(Schedulers.io());
+
     }
 
     private Observable<Integer> task1() {
         //  1. 創建被觀察者 Observable
         return Observable.create(new ObservableOnSubscribe<Integer>() {
-        // 2. 在subscribe（）裡定義需要被發送的事件，時間到了，api回來了....
+            // 2. 在subscribe（）裡定義需要被發送的事件，時間到了，api回來了....
             @Override
             public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
                 // 通过 ObservableEmitter类对象产生事件并通知观察者
@@ -120,19 +127,20 @@ public class MainActivity extends AppCompatActivity {
             if (value.toString().equals("15")) {
                 Log.d(TAG, "15秒到了");
             }
+            Log.d(TAG, "value" + value);
             // mDisposable.dispose(); //切斷观察者和被观察者的连接
         }
 
         @Override
         public void onError(Throwable e) {
-            Log.d(TAG, "对Error事件作出响应");
+            Log.d(TAG, "走了 onError");
         }
 
         @Override
         public void onComplete() {
-            Log.d(TAG, "对Complete事件作出响应");
+            Log.d(TAG, "就不會走 onComplete，反之");
+            latch.countDown();
         }
-
     };
 
 }
